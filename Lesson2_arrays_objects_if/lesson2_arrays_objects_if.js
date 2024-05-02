13bdfa3c570fa5e3089a7ccbb426d988f6752930
PatriciaMnd <mondapatricia27@gmail.com>
2024-05-02 04:38:46 +0300

    Create arrays and objects

---
+++ Lesson2_arrays_objects_if/lesson2_arrays_objects_if.js
@@ -0,0 +1,138 @@
+let alphabet= ['a','b','c','d','e','f','g','h','i','j'];
+console.log(alphabet[0])
+console.log(alphabet[1])
+console.log(alphabet[2])
+console.log(alphabet[3])
+console.log(alphabet[4])
+console.log(alphabet[5])
+console.log(alphabet[6])
+console.log(alphabet[7])
+console.log(alphabet[8])
+console.log(alphabet[9])
+
+// ---------------------------------------------------------------------------------------------------------------//
+
+let book1 = {
+    title:'1984',
+    pages:250,
+    genre: 'dystopian'
+}
+console.log(book1)
+
+let book2 = {
+    title:'Moby-Dick',
+    pages:300,
+    genre: 'adventure fiction'
+}
+console.log(book2)
+
+let book3 = {
+    title:'Don Quixote',
+    pages:500,
+    genre: 'novel'
+}
+console.log(book3)
+
+// ---------------------------------------------------------------------------------------------------------------//
+
+let book4 = {
+    title:'How to Save a Life: A novel',
+    pages:303,
+    genre: 'fiction',
+    authors:[{name:'Liz Fenton', age:25} ,{name:'Lisa Steinke', age:29}]
+}
+console.log(book4)
+
+let book5 = {
+    title:'Ribbons of Scarlet',
+    pages:560,
+    genre: 'historical fiction',
+    authors:[{name:'Kate Quinn', age:32} ,{name:'Stephanie Dray', age:45}, {name:'Laura Kamoie', age:40}]
+}
+console.log(book5)
+
+let book6 = {
+    title:'All the Ways We Said Goodbye',
+    pages:448,
+    genre: 'historical fiction',
+    authors:[{name:'Beatriz Williams', age:24},{name:'Lauren Willig', age:33}, {name:'Karen White', age:28}]
+}
+console.log(book6)
+
+// -----------------------------------------------------------------------------------------------------------------//
+
+let user1 = {
+    name: 'Daniel',
+    surname:'Williams',
+    password:1111
+}
+console.log(user1.password)
+let user2 = {
+    name: 'Susan',
+    surname:'Harris',
+    password:2222
+}
+console.log(user2.password)
+let user3 = {
+    name: 'Joseph',
+    surname:'Robinson',
+    password:3333
+}
+console.log(user3.password)
+let user4 = {
+    name: 'Linda',
+    surname:'Walker',
+    password:4444
+}
+console.log(user4.password)
+let user5 = {
+    name: 'David',
+    surname:'Scott',
+    password:5555
+}
+console.log(user5.password)
+let user6 = {
+    name: 'Betty',
+    surname:'Nelson',
+    password:6666
+}
+console.log(user6.password)
+let user7 = {
+    name: 'John',
+    surname:'Cooper',
+    password:7777
+}
+console.log(user7.password)
+let user8 = {
+    name: 'Margaret',
+    surname:'Miller',
+    password:8888
+}
+console.log(user8.password)
+let user9 = {
+    name: 'Gary',
+    surname:'Smith',
+    password:9999
+}
+console.log(user9.password)
+let user10 = {
+    name: 'Anna',
+    surname:'Stanley',
+    password:1010
+}
+console.log(user10.password)
+
+// ------------------------------------------------------------------------------------------------------------------//
+
+let weather = [
+    {day: 'Monday', morningTemp:+15,noonTemp:+23,eveningTemp:+17},
+    {day: 'Tuesday', morningTemp:+16,noonTemp:+24,eveningTemp:+19},
+    {day: 'Wednesday', morningTemp:+14,noonTemp:+21,eveningTemp:+16},
+    {day: 'Thursday', morningTemp:+14,noonTemp:+22,eveningTemp:+18},
+    {day: 'Friday', morningTemp:+16,noonTemp:+19,eveningTemp:+15},
+    {day: 'Saturday', morningTemp:+11,noonTemp:+15,eveningTemp:+14},
+    {day: 'Sunday', morningTemp:+10,noonTemp:+15,eveningTemp:+12}
+]
+console.log(weather)
+
+// ------------------------------------------------------------------------------------------------------------------//
